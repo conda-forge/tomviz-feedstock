e76c3bacd0d9f92f1798d00ed1e092394e61fee6
Patrick Avery <patrick.avery@kitware.com>
2022-01-05 12:25:02 -0600

    Include itk-elastix in tomviz packaging
    We want to include `itk-elastix` as a dependency to tomviz, but it is
    not available on conda-forge.
    
    However, it is available on pip, it is small, and it was designed to
    work in conda environments (similar to ITK). Thus, we can `pip install`
    it and include it in our tomviz package.
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -50,3 +50,11 @@ if errorlevel 1 exit 1
 
 cmake --build . --target install --config Release
 if errorlevel 1 exit 1
+
+:: Install itk-elastix.
+:: We'll include it in our package since it is not available on conda-forge.
+:: The latest ITK on conda-forge is 5.1.2, so install the latest
+:: itk-elastix that uses ITK 5.1.2, which is 0.9.0.
+set ITK_ELASTIX_WHL_URL=https://files.pythonhosted.org/packages/e3/15/7b727429e86967428f7354dc24022543086243720e9f7302ff94a3d741b3/itk_elastix-0.9.0-cp37-cp37m-win_amd64.whl
+pip install --no-deps %ITK_ELASTIX_WHL_URL%
+if errorlevel 1 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -44,3 +44,16 @@ cmake -G"Ninja" -DCMAKE_BUILD_TYPE:STRING=Release \
   -DPython3_ROOT_DIR:PATH=${PREFIX} \
   ../tomviz
 ninja install -j${CPU_COUNT}
+
+
+# Install itk-elastix. We'll include it in our package since it is not
+# available on conda-forge.
+# The latest ITK on conda-forge is 5.1.2, so install the latest
+# itk-elastix that uses ITK 5.1.2, which is 0.9.0.
+if [ `uname` == Darwin ]; then
+  ITK_ELASTIX_WHL_URL=https://files.pythonhosted.org/packages/46/a1/24311db277ce6432ac2c799fe77a01c5151a5b4376f15d088b7c265cc36f/itk_elastix-0.9.0-cp37-cp37m-macosx_10_9_x86_64.whl
+elif [ `uname` == Linux ]; then
+  ITK_ELASTIX_WHL_URL=https://files.pythonhosted.org/packages/48/a2/c93146a9f7c60026323b668dd0b12546afdff77f1dc14ecd75e0bbd95bb8/itk_elastix-0.9.0-cp37-cp37m-manylinux1_x86_64.whl
+fi
+
+pip install --no-deps $ITK_ELASTIX_WHL_URL
